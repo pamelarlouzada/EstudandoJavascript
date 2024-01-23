8b92b1904cb4daa2101edca9fcb93daa96ee955c
pamela.louzada <pamela.rlouzada@gmail.com>
2024-01-23 15:01:53 -0300

    setInterval - Iniciando o temporizador

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const startPauseBt = document.querySelector('#start-pause')
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null
+
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
@@ -16,7 +21,6 @@ musicaFocoInput.addEventListener('change', () => {
         musica.pause()
     }
 })
-
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco')
@@ -67,4 +71,17 @@ function alterarContexto (contexto) {
         default:
             break
     }
+}
+
+const contagemRegressiva = () => {
+    iniciar()
+    tempoDecorridoEmSegundos -= 1
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+}
+
+
+startPauseBt.addEventListener('click', contagemRegressiva)
+
+function iniciar() {
+    intervaloId = setInterval(contagemRegressiva, 1000)
 }
