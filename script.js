0ae7ffeecae658a69a5c13d89696f35a5e584211
pamela.louzada <pamela.rlouzada@gmail.com>
2024-01-25 16:05:59 -0300

    Formatando tempo com objeto Date

--- script.js
+++ script.js
@@ -33,21 +33,25 @@ musicaFocoInput.addEventListener('change', () => {
 })
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500
     alterarContexto('foco')
     focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300
     alterarContexto('descanso-curto')
     curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
 
 function alterarContexto (contexto) {
+    mostrarTempo()
     botoes.forEach(function (contexto) {
         contexto.classList.remove('active')
     })
@@ -118,8 +122,9 @@ function zerar(){
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
 function mostrarTempo() {
-    const tempo = tempoDecorridoEmSegundos 
-    tempoNaTela.innerHTML = `${tempo}`
+    const tempo = new Date(tempoDecorridoEmSegundos*1000)  ;
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second:'2-digit'} )
+    tempoNaTela.innerHTML = `${tempoFormatado}`
 }
 
 mostrarTempo();
