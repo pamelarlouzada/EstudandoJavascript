9fde22b9a84989cc27ffb21b4d698e0643513da6
pamela.louzada <pamela.rlouzada@gmail.com>
2024-01-25 16:47:55 -0300

    Consumindo a API Json

--- script-crud.js
+++ script-crud.js
@@ -15,5 +15,5 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
             descricao: textarea.value
     }
     tarefas.push(tarefa)
-    localStorage.setItem('tarefas', tarefas)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
