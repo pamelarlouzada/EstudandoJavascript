1e6acdb938cbada7b8f6b6ea4946bab8192841a5
Pamela Rodrigues Louzada <pamela.rlouzada@gmail.com>
2024-01-31 21:37:35 -0300

    Alterando a tarefa

--- script-crud.js
+++ script-crud.js
@@ -25,6 +25,10 @@ function criarElementoTarefa(tarefa) {
     
     const botao = document.createElement('button')
     botao.classList.add('app_button-edit')
+    botao.onclick = () => {
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        paragrafo.textContent = novaDescricao
+    }
 
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', '/imagens/edit.png')
