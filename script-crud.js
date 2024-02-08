a037e4c55a3264dea3cda7c732113a6256580606
Pamela Rodrigues Louzada <pamela.rlouzada@gmail.com>
2024-02-08 20:58:12 -0300

    Remove tarefas concluídas

--- script-crud.js
+++ script-crud.js
@@ -6,7 +6,9 @@ const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] 
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] 
 let tarefaSelecionada = null;
 let liTarefaSelecionada = null;
 
@@ -113,3 +115,12 @@ document.addEventListener('FocoFinalizado', () =>{
         atualizarTarefas()
     }
 })
+
+btnRemoverConcluidas.onclick = () => {
+    const seletor = ".app__section-task-list-item-complete"
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = tarefas.filter(tarefa => !tarefa.completa)
+    atualizarTarefas()
+}
